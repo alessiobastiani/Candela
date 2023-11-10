f6fc27aa508c40baf675bb82bd518e09cb07becc
alessiobastiani <ale-ssio@hotmail.es>
2023-11-10 18:57:16 -0300

    terminado

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -1,9 +1,11 @@
 // Loader.js
 import React from 'react';
+import logo from '../assets/logo2.png'
 
 const Loader = () => {
   return (
     <div className="loader-container">
+        <img className='logo-l' src={logo} alt="" />
       <div className="loader">
         <div className="scanner">
           <span>La Candela...</span>
